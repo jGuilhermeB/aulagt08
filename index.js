24c1b71d6a1a98350ff440723e9b0c44086e64ce
guilherme <jdasilveirabarroso@gmail.com>
2025-05-05 16:53:06 -0300

    inicio do filtro

---
+++ index.js
@@ -0,0 +1,60 @@
+const searchInput = document.getElementById('searchInput')
+
+const produtos= [
+    {
+        img: './assets/cafecomd.jpg',
+        titulo: 'café com deus pai',
+        categoria:'desenvolvimento pessoal',
+        preco: 69.99,
+        desconto: false,
+    },
+    {
+        img: './assets/cafecomd.jpg',
+        titulo: 'café com deus pai',
+        categoria:'desenvolvimento pessoal',
+        preco: 69.99,
+        desconto: true,
+    },
+    {
+        img: './assets/cafecomd.jpg',
+        titulo: 'café com deus pai',
+        categoria:'desenvolvimento pessoal',
+        preco: 69.99,
+        desconto: false,
+    },
+    {
+        img: './assets/cafecomd.jpg',
+        titulo: 'café com deus pai',
+        categoria:'desenvolvimento pessoal',
+        preco: 69.99,
+        desconto: true,
+    },
+    {
+        img: './assets/cafecomd.jpg',
+        titulo: 'café com deus pai',
+        categoria:'desenvolvimento pessoal',
+        preco: 69.99,
+        desconto: true,
+    },
+    {
+        img: './assets/cafecomd.jpg',
+        titulo: 'café com deus pai',
+        categoria:'desenvolvimento pessoal',
+        preco: 69.99,
+        desconto: false,
+    },
+    {
+        img: './assets/cafecomd.jpg',
+        titulo: 'café com deus pai',
+        categoria:'desenvolvimento pessoal',
+        preco: 69.99,
+        desconto: true,
+    },
+    {
+        img: './assets/cafecomd.jpg',
+        titulo: 'café com deus pai',
+        categoria:'desenvolvimento pessoal',
+        preco: 69.99,
+        desconto: false,
+    },
+]
