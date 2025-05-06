823ebc5bdeb76cfb442502598e6a61f319cabc43
guilherme <jdasilveirabarroso@gmail.com>
2025-05-06 16:40:47 -0300

    fetch

---
+++ cadastro.js
@@ -0,0 +1,12 @@
+const cep = document.getElementById('cep')
+const rua = document.getElementById('rua')
+const bairro = document.getElementById('bairro')
+const cidade = document.getElementById('cidade')
+const uf = document.getElementById('uf')
+
+
+function(buscarCep){
+    if(cep.value.length == 8){
+        fetch
+    }
+}
